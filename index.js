2e5317391715c96924dcbd69799d9bda11620940
Code Terra <76623307+codeterrayt@users.noreply.github.com>
2024-09-20 14:03:28 +0530

    update

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ app.set('view engine', 'ejs');
 
 // MySQL connection
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'my_database'
+    host: process.env.DATABASE_HOST || 'db',
+    user: process.env.DATABASE_USER || 'root',
+    password: process.env.DATABASE_PASSWORD || '',
+    database: process.env.DATABASE_NAME || 'my_database'
 });
 
 // Connect to MySQL and create table if not exists
