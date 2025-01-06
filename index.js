d5c4fb48c5472e8fa79152788341e0f39f61d37b
Code Terra <76623307+codeterrayt@users.noreply.github.com>
2025-01-06 21:51:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ app.get("/test", (req, res) => {
 // Home page route to display data
 app.get('/', (req, res) => {
 
-    return res.send("hi22");
+    return res.send("hi222");
     
     // const getUsersQuery = 'SELECT * FROM users';
     // db.query(getUsersQuery, (err, results) => {
