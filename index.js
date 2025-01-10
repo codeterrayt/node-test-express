b407ae94322566ecdeafb61bb6f3e3e1d93ccc7c
Code Terra <76623307+codeterrayt@users.noreply.github.com>
2025-01-10 17:14:54 +0530

    change2

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.set('view engine', 'ejs');
 // Home page route to display data s
 app.get('/', (req, res) => {
 
-    return res.send("rohan prajapati");
+    return res.send("rohan2");
     
     // const getUsersQuery = 'SELECT * FROM users'; s
     // db.query(getUsersQuery, (err, results) => {
